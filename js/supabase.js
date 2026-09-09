b5b5eea18ae9362cd957105559a9649d19658955
hisnationalanthem <potatolizard123@gmail.com>
2026-09-09 14:30:07 -0300

    Create Supabase JavaScript file

---
+++ js/supabase.js
@@ -0,0 +1,12 @@
+/* ========================================
+   HIS_NATIONAL_ANTHEM
+   Supabase Connection
+   ======================================== */
+
+
+/*
+   Supabase setup will be added later.
+
+   This file will handle the website's
+   connection to the Supabase database.
+*/

34b721cf206584d8ac026170ac64f4fbdfa60e94
hisnationalanthem <potatolizard123@gmail.com>
2026-09-09 14:34:36 -0300

    Create membership JavaScript

---
+++ js/membership.js
@@ -0,0 +1,12 @@
+/* ========================================
+   HIS_NATIONAL_ANTHEM
+   Membership System
+   ======================================== */
+
+
+/*
+   Membership credits, rollover,
+   member commission allowances,
+   and member dashboard functions
+   will go here.
+*/

96fc68d8411e0b6d50059f9fdc3b09b97f4374f3
hisnationalanthem <potatolizard123@gmail.com>
2026-09-09 14:29:12 -0300

    Create main site JavaScript

---
+++ js/site.js
@@ -0,0 +1,8 @@
+/* ========================================
+   HIS_NATIONAL_ANTHEM
+   Main Website JavaScript
+   ======================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("his_national_anthem website loaded");
+});

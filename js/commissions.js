691b5c22b45bcd96509acdf1d9a783f330171b2a
hisnationalanthem <potatolizard123@gmail.com>
2026-09-09 14:33:52 -0300

    Create commission JavaScript

---
+++ js/commissions.js
@@ -0,0 +1,12 @@
+/* ========================================
+   HIS_NATIONAL_ANTHEM
+   Commission System
+   ======================================== */
+
+
+/*
+   Commission forms, prices,
+   add-ons, submission logic,
+   and commission status functions
+   will go here.
+*/

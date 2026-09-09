ae353a0be38bed6c9380b1fb1be1eb9f1313c7ee
hisnationalanthem <potatolizard123@gmail.com>
2026-09-09 14:29:44 -0300

    Create website configuration

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/* ========================================
+   HIS_NATIONAL_ANTHEM
+   Website Configuration
+   ======================================== */
+
+const SITE_CONFIG = {
+    siteName: "his_national_anthem"
+};
